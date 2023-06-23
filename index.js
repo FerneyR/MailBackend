ca5709a78ccf3080212afee987286a13d172e819
Ferney Ramos <ferneyr543@gmail.com>
2023-06-22 23:21:22 -0500

    testx2

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-var bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const port = 8080;
-const router = require('./src/routes');
+const router = require("./src/routes");
 
-app.use(cors());
+app.use(cors()); // Habilitar CORS para todas las rutas
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Configurar el proxy inverso
+const apiProxy = createProxyMiddleware("/get", {
+  target: "https://hammerhead-app-bc22e.ondigitalocean.app",
+  changeOrigin: true,
+});
+
+app.use("/get", apiProxy);
+
 app.use(router);
 
-app.get('/', async (req, res) => {s
+app.get("/", async (req, res) => {
   res.send("Proyecto Post Office.");
 });
 
