2a2be59ecec874724a60a99b351022e6168985ad
Ferney Ramos <ferneyr543@gmail.com>
2023-06-22 22:39:24 -0500

    cors fdp

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require("express");
-const cors = require("cors")
-var bodyParser = require('body-parser')
+const cors = require("cors");
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 const router = require('./src/routes');
 
-app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: '200'
-}))
+app.use(cors()); // Habilitar CORS para todas las rutas
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(router);
 
 app.get('/', async (req, res) => {
-    res.send("Proyecto Post Office.")
+  res.send("Proyecto Post Office.");
 });
 
 app.listen(port, () => {
-    console.log("iniciado");
-})
+  console.log("Iniciado");
+});
